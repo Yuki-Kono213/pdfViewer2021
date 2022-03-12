2951da5119043ea08fab88bd0d659ffac120bae8
Yuki-Kono213 <marushige9685@gmail.com>
2022-03-12 21:59:34 +0900

    プロジェクト ファイルを追加します。

---
+++ pdfViewer2021/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace pdfViewer2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstpdfFile = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.lblLastUpdate = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(17, 16);
+            this.txtDirectory.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(356, 22);
+            this.txtDirectory.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "フォルダを参照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lstpdfFile
+            // 
+            this.lstpdfFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstpdfFile.FormattingEnabled = true;
+            this.lstpdfFile.ItemHeight = 15;
+            this.lstpdfFile.Location = new System.Drawing.Point(17, 86);
+            this.lstpdfFile.Margin = new System.Windows.Forms.Padding(4);
+            this.lstpdfFile.Name = "lstpdfFile";
+            this.lstpdfFile.Size = new System.Drawing.Size(356, 469);
+            this.lstpdfFile.TabIndex = 2;
+            this.lstpdfFile.SelectedIndexChanged += new System.EventHandler(this.lstpdfFile_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(384, 8);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(64, 15);
+            this.lblFileName.TabIndex = 5;
+            this.lblFileName.Text = "ファイル名";
+            // 
+            // lblLastUpdate
+            // 
+            this.lblLastUpdate.AutoSize = true;
+            this.lblLastUpdate.Location = new System.Drawing.Point(384, 30);
+            this.lblLastUpdate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastUpdate.Name = "lblLastUpdate";
+            this.lblLastUpdate.Size = new System.Drawing.Size(67, 15);
+            this.lblLastUpdate.TabIndex = 6;
+            this.lblLastUpdate.Text = "最終更新";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 49);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "文字列入力で表示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(387, 62);
+            this.axAcroPDF1.Margin = new System.Windows.Forms.Padding(4);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(500, 399);
+            this.axAcroPDF1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblLastUpdate);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.lstpdfFile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDirectory);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "pdfビューワー";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstpdfFile;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label lblLastUpdate;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ pdfViewer2021/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pdfViewer2021
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        //private async void lstpdfFile_Click(object sender, EventArgs e)
+        //{
+        //    // PDFファイルを開くためのピッカーを準備
+        //    var picker = new Windows.Storage.Pickers.FileOpenPicker();
+        //    picker.FileTypeFilter.Add(".pdf");
+        //    Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+        //    if (file != null)
+        //    {
+        //        try
+        //        {
+        //            // PDFファイルを読み込む
+        //            pdfDocument = await Windows.Data.Pdf.PdfDocument.LoadFromFileAsync(file);
+        //        }
+        //        catch
+        //        {
+
+        //        }
+        //    }
+
+        //    if (pdfDocument != null)
+        //    {
+        //        // 1ページ目を読み込む
+        //        using (Windows.Data.Pdf.PdfPage page = pdfDocument.GetPage(0))
+        //        {
+        //            // ビットマップイメージの作成
+        //            var stream = new Windows.Storage.Streams.InMemoryRandomAccessStream();
+        //            await page.RenderToStreamAsync(stream);
+        //            Windows.UI.Xaml.Media.Imaging.BitmapImage src = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+
+        //            // Imageオブジェクトにsrcをセット
+        //            imgPdf.Source = src;
+
+        //            // srcに作成したビットマップイメージを流し込む
+        //            await src.SetSourceAsync(stream);
+        //        }
+        //    }
+
+        //}
+
+        private void lstpdfFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string FileName = lstpdfFile.Items[lstpdfFile.SelectedIndex].ToString().Normalize();
+            lblLastUpdate.Text = "最終更新" + System.IO.File.GetLastWriteTime(FolderName + "\\" + FileName).ToString();
+            lblFileName.Text = FileName;
+            bool a = axAcroPDF1.LoadFile(FolderName + "\\" + FileName);
+            axAcroPDF1.LoadFile(FolderName + "\\" + FileName);
+
+        }
+        string FolderName;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbDialog = new FolderBrowserDialog();
+
+            // ダイアログの説明文を指定する
+            fbDialog.Description = "ダイアログの説明文";
+
+            // デフォルトのフォルダを指定する
+            fbDialog.SelectedPath = @"C:";
+            //フォルダを選択するダイアログを表示する
+            if (fbDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                Console.WriteLine(fbDialog.SelectedPath);
+                //フォルダー名を取得
+                FolderName = fbDialog.SelectedPath;
+                txtDirectory.Text = FolderName;
+                //リストボックスをクリア
+                lstpdfFile.Items.Clear();
+                //リストボックスに水平スクロールバーを表示
+                lstpdfFile.HorizontalScrollbar = true;
+
+                //フォルダー名が取得できたら
+                if (FolderName.Length > 1)
+                {
+                    //指定の拡張子のファイルだけ取得する場合
+                    foreach (string FileName in System.IO.Directory.GetFiles(FolderName, "*.pdf"))
+                    {
+                        lstpdfFile.Items.Add(Path.GetFileName(FileName));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("キャンセルされました");
+            }
+
+
+
+
+            // オブジェクトを破棄する
+            fbDialog.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderName = txtDirectory.Text;
+            txtDirectory.Text = FolderName;
+            //リストボックスをクリア
+            lstpdfFile.Items.Clear();
+            //リストボックスに水平スクロールバーを表示
+            lstpdfFile.HorizontalScrollbar = true;
+
+            //フォルダー名が取得できたら
+            if (FolderName.Length > 1)
+            {
+                //指定の拡張子のファイルだけ取得する場合
+                foreach (string FileName in System.IO.Directory.GetFiles(FolderName, "*.pdf"))
+                {
+                    lstpdfFile.Items.Add(Path.GetFileName(FileName));
+                }
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (axAcroPDF1 != null)
+            {
+                // This purposely SUPPRESSES the Dispose of _adobeReader that otherwise automatically happens
+                // as the window shuts down. For some reason Dispose of this object is VERY slow; around 16 seconds
+                // on my fast desktop. I don't think it can hang on to any important resources once the app
+                // quits, so just prevent the Dispose.
+                // (I tried various other things, such as loading a non-existent file and catching the resulting
+                // exception, hiding the _adobeReader, Disposing it in advance (in this method)...nothing else
+                // prevented the long delay on shutdown.)
+                this.Controls.Remove(axAcroPDF1);
+                axAcroPDF1 = null;
+            }
+
+
+        }
+
+    }
+}
